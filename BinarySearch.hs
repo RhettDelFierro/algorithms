11faf136bc79c5980391cbd5a44866fa856c2e30
Rhett Del Fierro <thinquetank@gmail.com>
2017-07-25 14:07:47 -0700

    higher than mid is working, but edge cases on testing throw errors or bottom.

--- BinarySearch.hs
+++ BinarySearch.hs
@@ -1,6 +1,6 @@
 binarySearch xs find low high
              | find == guess' = Just mid --end goal this is fine.
-             | find > guess'  = binarySearch xs find (mid + 1) high
+             | find > guess'  = binarySearch xs find mid high
              | find < guess'  = binarySearch xs find low (mid - 1)
              | otherwise      = Nothing
              where high'  = high - 1
@@ -8,10 +8,20 @@ binarySearch xs find low high
                    guess' = xs !! mid
 
 {- tests
+list = [1,2,3,4,5,6,7]
+
 *Main> binarySearch list 4 0 (length list)
 Just 3
 
 *Main> binarySearch list 2 0 (length list)
 Just 1
 
+*Main> binarySearch list 6 0 (length list)
+Just 5
+*Main> binarySearch list 0 0 (length list)
+*** Exception: Prelude.!!: negative index
+
+BOTTOM:
+*Main> binarySearch list 7 0 (length list)
+^CInterrupted.
 -}
