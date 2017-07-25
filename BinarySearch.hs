d96d631c4c4f8befdd392696bb50b5a868112324
Rhett Del Fierro <thinquetank@gmail.com>
2017-07-25 14:03:03 -0700

    lower than mid is working

--- BinarySearch.hs
+++ BinarySearch.hs
@@ -1,13 +1,17 @@
 binarySearch xs find low high
-             | find == guess' = Just mid' --end goal this is fine.
-             | find > guess'  = binarySearch xs find low (mid' + 1)
-             | find < guess'  = binarySearch xs find (mid' - 1) high
+             | find == guess' = Just mid --end goal this is fine.
+             | find > guess'  = binarySearch xs find (mid + 1) high
+             | find < guess'  = binarySearch xs find low (mid - 1)
              | otherwise      = Nothing
              where high'  = high - 1
-                   mid'   = div (low + high') 2
-                   guess' = xs !! mid'
+                   mid   = div (low + high') 2
+                   guess' = xs !! mid
 
 {- tests
 *Main> binarySearch list 4 0 (length list)
 Just 3
+
+*Main> binarySearch list 2 0 (length list)
+Just 1
+
 -}
